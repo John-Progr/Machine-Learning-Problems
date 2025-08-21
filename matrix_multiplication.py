a2f898f7b0c750fbfb4413ab4d8fe48c89ee17d0
John Matsoukas <62017772+John-Progr@users.noreply.github.com>
2025-08-21 23:25:20 +0300

    Create matrix_multiplication.py

---
+++ matrix_multiplication.py
@@ -0,0 +1,9 @@
+def scalar_multiply(matrix: list[list[int|float]], scalar: int|float) -> list[list[int|float]]:
+  
+  result = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+  
+  for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+      result[i][j] = scalar * matrix[i][j]
+  return result
+	
